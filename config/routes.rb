7d4e67cc08c928d6ba96d361ea26266a2f88b614
河合盛宇 <kawaimorisakai@kawaimorisakainoMacBook-Pro.local>
2019-01-03 17:04:19 +0900

    add edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'posts/:id' => 'posts#show'
   get '/' => 'home#top'
   get 'about' => 'home#about'
+  get 'posts/:id/edit'=> 'posts#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'posts/create' => 'posts#create'
+  post 'posts/:id/update' => 'posts#update'
 end
