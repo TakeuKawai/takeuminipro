e49cb745468af0d1c2b726b19d6318431b1cedbc
河合盛宇 <kawaimorisakai@kawaimorisakainoMacBook-Pro.local>
2019-01-04 01:52:50 +0900

    completed edit add destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'posts/create' => 'posts#create'
   post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
 end
